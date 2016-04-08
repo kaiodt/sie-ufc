65e19818982e2c7f1cf37fdf46b91a65caaba8a4
lucassm <lucas.silveiramelo@gmail.com>
2016-04-08 00:35:08 -0300

    database table Usuarios added

--- main.py
+++ main.py
@@ -5,8 +5,19 @@ from flask.ext.sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://ybygigmtetzayp:d-nF5hqgyp4h52VuciHozv8pPF@ec2-54-225-111-9.compute-1.amazonaws.com:5432/d6sbmnjm5hu4v8'
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+db = SQLAlchemy(app)
 manager = Manager(app)
 bootstrap = Bootstrap(app)
+
+class Usuario(db.Model):
+    __tablename__ = 'usuarios'
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(64), unique=True)
+
+    def __repr__(self):
+        return '<Usuario %r>' % self.nome
 
 @app.route('/')
 def index():
